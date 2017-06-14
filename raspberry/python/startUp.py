7215d4f645ae949ede63c531c29bed1742be2753
Antti Tuovinen <k2470@student.jamk.fi>
2017-06-14 11:12:53 +0300

    Update startUp.py

--- raspberry/python/startUp.py
+++ raspberry/python/startUp.py
@@ -33,7 +33,7 @@ def SendData(data):
   data['ip'] = getIp()
   data['firmwarever'] = '0.25'
   client.publish('raspi', json.dumps(data))
-  f = open('data.json' 'w+')
+  f = open('data.json', 'w+')
   f.write(json.dumps(data))
   f.close()
 
