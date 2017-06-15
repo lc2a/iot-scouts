a257d6197e0d8e45b9f7b6c42156955e98ab6814
Antti Tuovinen <k2470@student.jamk.fi>
2017-06-15 13:31:55 +0300

    Update startUp.py

--- raspberry/python/startUp.py
+++ raspberry/python/startUp.py
@@ -33,9 +33,10 @@ def SendData(data):
   data['ip'] = getIp()
   data['firmwarever'] = '0.25'
   client.publish('raspi', json.dumps(data))
-  f = open('/home/pi/data/data.json', 'w+')
-  f.write(str(json.dumps(data)))
-  f.close()
+  if serialNumber == "raspi-o827ro544093":
+    f = open('/home/pi/data/data.json', 'w+')
+    f.write(str(json.dumps(data)))
+    f.close()
 
 
 def printPixels():
