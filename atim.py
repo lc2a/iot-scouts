3ba2b14f1713cf8da400dcbf663651876e26c73d
Antti Tuovinen <k2470@student.jamk.fi>
2017-07-03 16:03:43 +0300

    added posting data to tb

--- atim.py
+++ atim.py
@@ -1,7 +1,4 @@
-#Gets data from atim api and posts it to our database, usage: python3 atim.py -u email_for_atim -p pass_for_atim
-#working: authentication, parsing data to json format ready
-#todo: post data to db
-
+#Gets data from atim api and posts it to our database, usage: python3 atim.py -u email_for_atim -p pass_for_atim -at access_token -ip ip_to_tb
 import sys
 import datetime
 import pytz
@@ -14,14 +11,18 @@ import argparse
 import re
 import email.utils
 import base64
+import paho.mqtt.client as mqtt
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-u", help="email for atim api",type=str, required=True)
 parser.add_argument("-p", help="password for atim api",type=str, required=True)
+parser.add_argument("-at", help="access token for the mqtt gateway in thingsboard",type=str, required=True)
+parser.add_argument("-ip", help="ip to thingsboard",type=str, required=True)
 
 args = parser.parse_args()
 
 auth_code = base64.b64encode('{}:{}'.format(args.u, args.p).encode()).decode()
+
 
 def convert2epoch(timetoconvert, timezone):
     d1 = datetime.datetime.strptime(timetoconvert,"%Y-%m-%dT%H:%M:%S.000Z")
@@ -38,23 +39,33 @@ headers = {'Authorization': 'Basic {}'.format(auth_code)}
 r = requests.get(url, headers=headers)
 devices = r.json()
 
+client = mqtt.Client()
+client.username_pw_set(username=args.at)
+client.connect(args.ip, 1883, 60)
+client.loop_start()
+
 for device in devices:
     
     if device['devicename'][0:3] == "Sig":
-        url2="https://api.atim.com/device/{}/channel/DIGITAL%20TEMPERATURE%201/message?limit=10".format(device['deviceid'])
+        url2="https://api.atim.com/device/{}/channel/DIGITAL%20TEMPERATURE%201/message".format(device['deviceid'])
         r = requests.get(url2, headers=headers)
         channeldatas = r.json()
         attributes_to_db = {}
         attributes_to_db['longitude'] = device['longitude']
         attributes_to_db['latitude'] = device['latitude']
         attributes_to_db['devicename'] = device['devicename']
-        attributes_to_db['serialNumber'] = "sig-{}".format(device['deviceid'])
-        #send this to thingsboard as attributes
+        serialNumber = "sig-{}".format(device['deviceid'])
+        attributes_to_db['serialNumber'] = serialNumber
+        client.publish('v1/gateway/connect', json.dumps({'device': serialNumber}))
+        client.publish('v1/gateway/attributes', json.dumps(attributes_to_db))
+        
         print(attributes_to_db)
+
         for channeldata in channeldatas:
             values = {}
             values['temperature'] = channeldata['value']
-            values['serialNumber'] = "sig-{}".format(device['deviceid'])
-            values_to_db = {'ts':convert2epoch(channeldata['time'], 'Europe/Helsinki'), 'values': values}
-            #send this to thingsboard as telemetry
+            values_to_db = {serialNumber: [{'ts':convert2epoch(channeldata['time'], 'Europe/Helsinki'), 'values': values}]}   
+            client.publish('v1/gateway/telemetry', json.dumps(values_to_db))
             print(values_to_db)
+
+        client.publish('v1/gateway/disconnect', json.dumps({'device': serialNumber}))
