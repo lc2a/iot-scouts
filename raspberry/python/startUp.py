43971e1a6fb3d3b5169cb431f96baec07de0700b
Lauri Korte <h8805@student.jamk.fi>
2017-07-06 15:26:54 +0300

    Update startUp.py

--- raspberry/python/startUp.py
+++ raspberry/python/startUp.py
@@ -84,7 +84,8 @@ def on_message(client, userdata, msg):
   print(msg.topic+" "+str(msg.payload))
   payload = json.loads(msg.payload.decode("utf-8"))
   sense.show_message(payload['message'], text_colour=red)
-
+  if (payload['message'] == 'LowLight'):
+    sense.low_light = (not sense.low_light)
 
 
 print("Waiting for internet access...")
