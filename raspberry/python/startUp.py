6bc3b75dc9ae38e6ab1fb4f8af41712a7a46173f
Antti Tuovinen <k2470@student.jamk.fi>
2017-07-06 15:02:38 +0300

    added rpc message capabilities

--- raspberry/python/startUp.py
+++ raspberry/python/startUp.py
@@ -23,73 +23,76 @@ args = parser.parse_args()
 HOST = args.ip
 
 try:
-	with open('/sys/class/net/eth0/address') as f:
-		serialNumber = "raspi-"+codecs.encode(f.read().strip().replace(":", ""), "rot-13")
+  with open('/sys/class/net/eth0/address') as f:
+    serialNumber = "raspi-"+codecs.encode(f.read().strip().replace(":", ""), "rot-13")
 except: 
     print('Failed to read MAC address')
 
 def SendData(data):
-	global serialNumber, client
-	data['serialNumber'] = serialNumber
-	data['ip'] = getIp()
-	data['firmwarever'] = '0.35'
-	client.publish('raspi', json.dumps(data))
-	try:
-		f = open('/home/pi/data/data.json', 'w+')
-		f.write(str(json.dumps(data)))
-		f.close()
-	except:
-		print("failed to save sensor data locally")
+  global serialNumber, client
+  data['serialNumber'] = serialNumber
+  data['ip'] = getIp()
+  data['firmwarever'] = '0.35'
+  client.publish('raspi', json.dumps(data))
+  try:
+    f = open('/home/pi/data/data.json', 'w+')
+    f.write(str(json.dumps(data)))
+    f.close()
+  except:
+    print("failed to save sensor data locally")
 
 
 def printPixels():
-	X = [0, 255, 0]  # Green
-	O = [0, 0, 0] # White
+  X = [0, 255, 0]  # Green
+  O = [0, 0, 0] # White
 
-	pixel_fin = [
-	X, O, O, O, O, O, O, X,
-	O, O, O, O, O, O, O, O,
-	O, O, O, O, O, O, O, O,
-	O, O, O, X, X, O, O, O,
-	O, O, O, X, X, O, O, O,
-	O, O, O, O, O, O, O, O,
-	O, O, O, O, O, O, O, O,
-	X, O, O, O, O, O, O, X
-	]
-	sense.set_pixels(pixel_fin)
-	time.sleep(2)
-	sense.clear()
+  pixel_fin = [
+  X, O, O, O, O, O, O, X,
+  O, O, O, O, O, O, O, O,
+  O, O, O, O, O, O, O, O,
+  O, O, O, X, X, O, O, O,
+  O, O, O, X, X, O, O, O,
+  O, O, O, O, O, O, O, O,
+  O, O, O, O, O, O, O, O,
+  X, O, O, O, O, O, O, X
+  ]
+  sense.set_pixels(pixel_fin)
+  time.sleep(2)
+  sense.clear()
 
 red = (255, 0, 0)
 green = (0, 255, 0)
 
 def getIp():
 # Try to get wlan0, otherwise eth0
-	global client
-	try:
-		ni.ifaddresses('eth0')
-		ip = ni.ifaddresses('eth0')[2][0]['addr']
-	except:
-		ni.ifaddresses('wlan0')
-		ip = ni.ifaddresses('wlan0')[2][0]['addr']
-	return ip
+  global client
+  try:
+    ni.ifaddresses('wlan0')
+    ip = ni.ifaddresses('wlan0')[2][0]['addr']
+  except:
+    ni.ifaddresses('eth0')
+    ip = ni.ifaddresses('eth0')[2][0]['addr']
+    return ip
 
 def getCPUtemp():
-	temp = subprocess.getoutput("/opt/vc/bin/vcgencmd measure_temp")
-	temp = temp.replace("temp=", "")
-	temp = temp.replace("'C", "")
-	return temp
+  temp = subprocess.getoutput("/opt/vc/bin/vcgencmd measure_temp")
+  temp = temp.replace("temp=", "")
+  temp = temp.replace("'C", "")
+  return temp
 
 def on_message(client, userdata, msg):
-	print(msg.topic+" "+str(msg.payload))
+  print(msg.topic+" "+str(msg.payload))
+  payload = json.loads(msg.payload.decode("utf-8"))
+  sense.show_message(payload['message'], text_colour=red)
+
 
 
 print("Waiting for internet access...")
 start = time.time()
 end = time.time()
 while end-start < 1:
-	sense.show_message("Connecting...", text_colour=red)
-	end = time.time()
+  sense.show_message("Connecting...", text_colour=red)
+  end = time.time()
 sense.show_message(" OK ", text_colour=green)
 
 
@@ -103,38 +106,33 @@ client.username_pw_set(username=args.user,password=args.passw)
 # Connect to Thingsboard using default MQTT port and 60 seconds keepalive interval
 client.connect(HOST, 1883, 60)
 client.loop_start()
-client.subscribe('sensor/' + serialNumber + '/request/+/+')
+client.subscribe('sensor/{}/request/#'.format(serialNumber))
 
 try:
-	count = 0
-	while True:
-		printPixels()
-		getCPUtemp()
-		humidity = round(sense.get_humidity(), 2)
-		temperature = round(sense.get_temperature(), 2)
-		air_pressure = round(sense.get_pressure(), 2)
-		print(u"Temperature: {:g}\u00b0C, Humidity: {:g}%, Pressure: {}hpa".format(temperature, humidity, air_pressure))
-		sensor_data['temperature'] = temperature
-		sensor_data['humidity'] = humidity
-		sensor_data['pressure'] = air_pressure
-		sensor_data['CPUtemp'] = getCPUtemp()
+  count = 0
+  while True:
+    printPixels()
+    getCPUtemp()
+    humidity = round(sense.get_humidity(), 2)
+    temperature = round(sense.get_temperature(), 2)
+    air_pressure = round(sense.get_pressure(), 2)
+    print(u"Temperature: {:g}\u00b0C, Humidity: {:g}%, Pressure: {}hpa".format(temperature, humidity, air_pressure))
+    sensor_data['temperature'] = temperature
+    sensor_data['humidity'] = humidity
+    sensor_data['pressure'] = air_pressure
+    sensor_data['CPUtemp'] = getCPUtemp()
 
-		if 10 < air_pressure < 1500:
-			#Sending humidity and temperature data to Thingsboard
-			SendData(sensor_data)
+    if 10 < air_pressure < 1500:
+      #Sending humidity and temperature data to Thingsboard
+      SendData(sensor_data)
 
-		next_reading += INTERVAL
-		sleep_time = next_reading-time.time()
+    next_reading += INTERVAL
+    sleep_time = next_reading-time.time()
 
-		count += 1
-		if count > 10:
-			getIp()
-			count = 0
-
-		if sleep_time > 0:
-			time.sleep(sleep_time)
+    if sleep_time > 0:
+      time.sleep(sleep_time)
 except KeyboardInterrupt:
-	pass
+  pass
 
 client.loop_stop()
 client.disconnect()
